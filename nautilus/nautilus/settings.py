b20796e0e7d567dc38da680983219c5fc776ea10
ericzzy <ericzhuangzhaoyi@outlook.com>
2023-03-08 20:10:05 -0500

    add allowed hosts

--- nautilus/nautilus/settings.py
+++ nautilus/nautilus/settings.py
@@ -127,6 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ALLOWED_HOSTS = [
     'zhaoyi-nautilus.netlify.app',
+    'zhaoyi-nautilus.herokuapp.com',
     'localhost',
     '127.0.0.1',
 ]
