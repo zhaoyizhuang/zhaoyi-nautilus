fecdcd28076173e1d414543e53714bdc681612ff
ericzzy <ericzhuangzhaoyi@outlook.com>
2023-03-08 19:49:17 -0500

    update prod url

--- nautilus-frontend/src/services/api.js
+++ nautilus-frontend/src/services/api.js
@@ -4,7 +4,7 @@ import Cookies from 'js-cookie'
 const isProduction = process.env === 'production'
 
 export default axios.create({
-  baseURL: isProduction ? 'prodUrl' : 'http://127.0.0.1:8000',
+  baseURL: isProduction ? 'https://zhaoyi-nautilus.herokuapp.com/' : 'http://127.0.0.1:8000',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
